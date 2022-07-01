3c370c5d6e22634a711dde855aba172469aef636
Inna Osipova <rutheva_inna@mail.ru>
2022-07-01 20:56:41 +0300

    Выбор номер четверти, нужно вспомнить Swich case

---
+++ Ex002/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая по заданному номеру четверти, показывает
+// диапазон возможных координат точек в этой четверти (x и y).
+
+string ShowCoord ( int number)
+{
+    if (number == 1) return " x > 0 и у >0";
+    if (number == 2) return " x < 0 и у >0";
+    if (number == 3) return " x < 0 и у <0";
+    if (number == 4) return " x > 0 и у < 0";
+    return " нули";
+}
+
+Console.Write ("Введите номер четверти : ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(ShowCoord(num));
