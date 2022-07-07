b464d1bda1f103d70a56276a7ce64e2ec3a1b074
Inna Osipova <rutheva_inna@mail.ru>
2022-07-08 01:06:01 +0300

    умножение

---
+++ Ex009/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задача 35: Задайте одномерный массив из 123 случайных чисел. 
+//Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+int[] Randomarray ()
+{
+    int[] array1 = new int [123];
+    for( int i = 0; i< array1.Length; i++)
+    {
+        array1[i] = new Random().Next(-100,1000);
+    }
+    return array1;
+}
+ 
+    int kol = 0;
+    
+    foreach(int item in Randomarray())
+    {
+        if (item >= 10 ) 
+        {   if(item <= 99)
+            { 
+               kol++;  
+            }
+        }
+    }
+
+    Console.WriteLine($" количество {kol}");
+
+    
---
+++ Ex010/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задача 37: Найдите произведение пар чисел в одномерном массиве. 
+//Парой считаем первый и последний элемент, второй и предпоследний и т.д. 
+//Результат запишите в новом массиве.
+
+
+int[] Randomarray ()
+{
+    int[] array1 = new int [11];
+    for( int i = 0; i< array1.Length; i++)
+    {
+        array1[i] = new Random().Next(-10,10);
+        Console.Write ("  "+array1[i]);
+    }
+    return array1;
+}
+   
+    int[] array = Randomarray();
+     Console.WriteLine();
+    for (int i  = 0; i < array.Length ; i++)
+    {
+        for ( int j = array.Length-1-i; j >=i; j--)
+        {
+           Console.WriteLine($"{array[i]} * {array[j]} = {array[i]*array[j]}");
+           break;
+        }
+    
+    }
+    
+    
+   
+    
