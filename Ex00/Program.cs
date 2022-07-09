8b93ba4085f25a9a157d6fc96536eec671d938ce
Inna Osipova <rutheva_inna@mail.ru>
2022-07-09 10:39:56 +0300

    таблица квадраов чисел

---
+++ Ex00/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая принимает на вход число (N) и выдаёт таблицу 
+//квадратов чисел от 1 до N.
+
+int Sqrt2 (int num)
+{
+    //int count = 0;
+   // int[] array = new int [num-1];
+    // while (count < num)
+    // {
+       int a  = num * num;
+        //Console.WriteLine(array[count]);
+     //   count++;
+    // }
+     return a;
+}
+
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int index =1;
+while(index <= number)
+{
+    Console.WriteLine(Sqrt2(index));
+    index++;
+}
+
+
+
