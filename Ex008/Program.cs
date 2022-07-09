f8f98b0fdc3b68fe8ef07854c935d4aa78f0f0f5
Inna Osipova <rutheva_inna@mail.ru>
2022-07-09 10:35:10 +0300

    поск числа в массиве, замена на противопол знак

---
+++ Ex008/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача 32: Напишите программу замена элементов массива: 
+//положительные элементы замените на соответствующие отрицательные, и наоборот.
+
+//void  ChengeArray (int[] arr)
+//{
+//    for (int i=0 ; i< arr.Length; i++)
+//    {
+//        arr[i]= arr[i]*(-1); 
+//        Console.Write(" "+arr[i]);
+//    }
+      
+//}
+
+//int[] array = new int[] { 1, -3, 5, -7, 9 }; 
+//ChengeArray(array);
+
+
+
+
+
+
+//Задача 33: Задайте массив. Напишите программу, которая определяет, 
+//присутствует ли заданное число в массиве.
+
+string  Surchnumber (int[] arr,  int num)
+{
+   for (int i=0 ; i< arr.Length; i++)
+    {
+        
+        if (arr[i] == num)
+        {
+            
+            return "присуиствует";
+            
+        }
+    }
+      return "отсутствует";
+}
+
+int[] array = new int[] { 1, -3, 5, -7, 9 }; 
+Console.Write ("Введите искомое число  :");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write(Surchnumber(array, number));
