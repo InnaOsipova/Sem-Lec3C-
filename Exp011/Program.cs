5432d2d5e0f60d5b37d635625c255b09e6d85058
Inna Osipova <rutheva_inna@mail.ru>
2022-07-09 10:36:30 +0300

    умножение массива

---
+++ Exp011/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] Randomarray ()
+{
+    int[] array1 = new int [11];
+    for( int i = 0; i< array1.Length; i++)
+    {
+        array1[i] = new Random().Next(-10,10);
+        Console.Write ("  "+array1[i]);
+    }
+    return array1;
+}
+   
+    int[] array = Randomarray();
+    int[] transarray = Randomarray();
+    Array.Reverse(transarray); 
+    Console.WriteLine();
+    for (int i  = 0; i < array.Length/2 ; i++)
+    {
+           Console.WriteLine($"{array[i]} * {transarray[i]} = {array[i]*transarray[i]}");
+        
+    
+    }
+    
+    
+   
